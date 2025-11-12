f83b7d897a5a798a72630e0c02a11a8bb4e5eb92
Dopa <ouaischeuwsh@gmail.com>
2025-11-12 22:49:09 +0100

    Create convert.js

---
+++ api/convert.js
@@ -0,0 +1,40 @@
+import fetch from "node-fetch";
+import { spawn } from "child_process";
+import stream from "stream";
+
+export const config = {
+  runtime: "nodejs18.x",
+};
+
+export default async function handler(req, res) {
+  const { url } = req.query;
+  if (!url) return res.status(400).send("No URL provided");
+
+  try {
+    if (!/^https?:\/\//.test(url)) {
+      return res.status(400).send("Invalid URL");
+    }
+
+    const ffmpeg = spawn("ffmpeg", [
+      "-i", url,
+      "-c:v", "libx264",
+      "-c:a", "aac",
+      "-f", "mp4",
+      "-movflags", "frag_keyframe+empty_moov",
+      "pipe:1",
+    ]);
+
+    res.setHeader("Content-Type", "video/mp4");
+
+    ffmpeg.stdout.pipe(res);
+    ffmpeg.stderr.on("data", (d) => console.log("FFmpeg:", d.toString()));
+
+    ffmpeg.on("error", (err) => {
+      console.error("FFmpeg error:", err);
+      res.status(500).send("FFmpeg error");
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+}
